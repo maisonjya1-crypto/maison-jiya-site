d62e9b230f6714a9cbba4c8aae78287d25d0d257
maisonjya1-crypto <maisonjya1@gmail.com>
2026-08-21 21:50:37 +0100

    Brancher l'app privée Maison Jiya Gestion

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import "./carrier-mode.css";
 import "./storefront-cms.css";
 import "./storefront-cms-v2.css";
 import "./best-seller-cms.css";
+import "./private-pwa.css";
 import DashboardClient from "./dashboard-client";
 import PlatformEnhancements from "./platform-enhancements";
 import CarrierModeEnhancement from "./carrier-mode-enhancement";
 import StorefrontCmsV2Enhancement from "./storefront-cms-v2-enhancement";
 import BestSellerCmsEnhancement from "./best-seller-cms-enhancement";
+import PrivatePwa from "./private-pwa";
 
 export default function Home() {
   return <>
@@ -17,5 +19,6 @@ export default function Home() {
     <CarrierModeEnhancement />
     <StorefrontCmsV2Enhancement />
     <BestSellerCmsEnhancement />
+    <PrivatePwa />
   </>;
 }
