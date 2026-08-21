0c1409b56e13c11d8c17d2d695eeeada4ef3eba9
maisonjya1-crypto <maisonjya1@gmail.com>
2026-08-21 19:16:59 +0100

    Corriger le typage DOM du CMS boutique

--- app/storefront-cms-dom.d.ts
+++ app/storefront-cms-dom.d.ts
@@ -3,5 +3,6 @@ export {};
 declare global {
   interface Document {
     querySelector(selectors: ".workspace"): HTMLElement | null;
+    querySelector<E extends Element = Element>(selectors: string): E | null;
   }
 }
